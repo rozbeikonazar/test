125ba651a4c6510df38a0d67d81c59b789e09dde
Nazar <qwerty2004123@gmail.com>
2023-06-08 22:39:53 +0200

    Added Celery task for setting subscription price and optimizing performance

--- service/services/views.py
+++ service/services/views.py
@@ -1,6 +1,5 @@
-from django.db.models import Prefetch, F, Sum
+from django.db.models import Prefetch, Sum
 from rest_framework.viewsets import ReadOnlyModelViewSet
-
 from clients.models import Client
 from services.models import Subscription
 from services.serializers import SubscriptionSerializer
@@ -10,9 +9,7 @@ class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related(
         'plan',
         Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email'))
-    ).annotate(price=F('service__full_price') -
-                     F('service__full_price') * F('plan__discount_percent') / 100.00
-               )
+    )
     serializer_class = SubscriptionSerializer
 
     def list(self, request, *args, **kwargs):
