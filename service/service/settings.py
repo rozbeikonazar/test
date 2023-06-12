8588db57d21a52c477ee015b1d70073f805a98a0
Nazar <qwerty2004123@gmail.com>
2023-06-13 00:31:49 +0200

    Added manual cache for total price

--- service/service/settings.py
+++ service/service/settings.py
@@ -146,3 +146,14 @@ LOGGING = {
 }
 
 CELERY_BROKER_URL = 'redis://redis:6379/0'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://redis:6379/0",
+        "OPTIONS": {
+            "db": "1",
+        },
+    }
+}
+PRICE_CACHE_NAME = 'price_cache'
